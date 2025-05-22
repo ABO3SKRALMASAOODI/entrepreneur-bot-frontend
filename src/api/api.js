aa569a2fcd3f52ec48aef91f25dc46a3d422fae8
MUSLIM <muslimtariq0@gmail.com>
2025-05-22 21:36:26 +0400

    Update API baseURL to live backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001", // Flask backend
+  baseURL: "https://entrepreneur-bot-backend.onrender.com",
+
 });
 
 export default API;
