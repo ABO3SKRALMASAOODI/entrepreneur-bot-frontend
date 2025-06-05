7b3e356f7b7cdff79685bddbd4faae9a0cd74c3e
MUSLIM <muslimtariq0@gmail.com>
2025-06-05 16:48:49 +0400

    Adjust robot and eye positioning for better alignment

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -121,7 +121,7 @@ function LandingPage() {
               </div>
             </div>
             <div
-            style={{ width: "100%", height: "100%" }}
+            style={{ width: "100%", height: "100%", marginTop: "20px" }}
              ref={lottieRef}
             >
            <Lottie animationData={robotAnimation} loop />
