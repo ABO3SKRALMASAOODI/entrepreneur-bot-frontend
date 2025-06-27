64db8f3bf23d86fd5414cfbef5442a5bf0dd370c
MUSLIM <muslimtariq0@gmail.com>
2025-06-27 20:27:15 +0400

    Your commit message

--- src/pages/CheckoutHandler.js
+++ src/pages/CheckoutHandler.js
@@ -3,18 +3,21 @@ import { useNavigate } from "react-router-dom";
 
 export default function CheckoutHandler() {
   const navigate = useNavigate();
-
   useEffect(() => {
+    if (window.Paddle) {
+      window.Paddle.Setup({ vendor: 232315 }); // Replace with your Paddle Vendor ID
+    }
+  
     const urlParams = new URLSearchParams(window.location.search);
     const transactionId = urlParams.get("_ptxn");
-
+  
     if (transactionId && window.Paddle) {
       window.Paddle.Checkout.open({ transactionId });
     } else {
       navigate("/"); // If no transaction, redirect home
     }
   }, [navigate]);
-
+  
   return (
     <div style={{ color: "#fff", textAlign: "center", marginTop: "4rem" }}>
       Loading checkout, please wait...
