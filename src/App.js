6e999ef43b4ce411bf7c17e8f452e778244c4200
MUSLIM <muslimtariq0@gmail.com>
2025-07-15 14:57:00 +0400

    Fix CORS for login with credentials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Legal from "./pages/legal";
 import VerifyCode from "./pages/VerifyCode";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
-import EnterPassword from "./pages/EnterPassword"; // ✅ Add this
+import EnterPassword from "./pages/EnterPassword";
 import ChangePassword from "./pages/ChangePassword";
 import ResetPassword from "./pages/ResetPassword";
 import Account from "./pages/Account";
@@ -21,32 +21,21 @@ function PrivateRoute({ children }) {
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/enter-password" element={<EnterPassword />} /> {/* ✅ NEW */}
-        <Route path="/register" element={<Register />} />
-        <Route path="/verify" element={<VerifyCode />} />
-        <Route path="/change-password" element={<ChangePassword />} />
-        
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
-        <Route path="/features" element={<FeaturesPage />} />
-        <Route path="/legal" element={<Legal />} />
-        <Route path="/paddle-checkout" element={<PaddleCheckoutPage />} />
-        <Route path="/subscribe" element={<SubscribePage />} />
-
-        <Route
-          path="/chat"
-          element={
-            <PrivateRoute>
-              <Chat />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/enter-password" element={<EnterPassword />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/verify" element={<VerifyCode />} />
+      <Route path="/change-password" element={<ChangePassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
+      <Route path="/features" element={<FeaturesPage />} />
+      <Route path="/legal" element={<Legal />} />
+      <Route path="/paddle-checkout" element={<PaddleCheckoutPage />} />
+      <Route path="/subscribe" element={<SubscribePage />} />
+      <Route path="/chat" element={<PrivateRoute><Chat /></PrivateRoute>} />
+    </Routes>
   );
 }
 
